826dbd7dd62603654a6d3d8ab128fdd0673cf8e7
Homura <egzozu.be1.bas@gmail.com>
2025-07-13 07:44:16 +0300

    I'm seriously tired

--- patches/musl/zig/lib/libc/musl/src/unistd/setregid.c
+++ patches/musl/zig/lib/libc/musl/src/unistd/setregid.c
@@ -5,7 +5,7 @@
 
 int setregid(gid_t rgid, gid_t egid)
 {
-        const char *is_android = getenv("ANDROID_DATA");
+	const char *is_android = getenv("ANDROID_DATA");
 	if (is_android)
 		return 0;
 	return __setxid(SYS_setregid, rgid, egid, 0);
